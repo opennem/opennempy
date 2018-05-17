b82d3b683bdebbafaee5fb0295320afea846a0ea
Dylan McConnell <dylan-climate-energy-college@users.noreply.github.com>
2018-05-17 11:51:12 +1000

    Added functions to load and save timeseries data

--- web_api.py
+++ web_api.py
@@ -6,22 +6,45 @@ import datetime
 import os
 
 data_dir = config.get("local_settings",'data_dir')
+live_url = "http://data.opennem.org.au/power/{0}.json"
+history_url = "http://data.opennem.org.au/power/history/5minute/{0}_{1}.json"
 
-def load_latest_week(region="sa1"):
-	r = requests.get("http://data.opennem.org.au/power/{0}.json".format(region))
+def live_week(region="sa1"):
+	r = requests.get(live_url.format(region))
 	df = pd.concat([json_to_pd(data_set) for data_set in data_sets(r)], axis=1)
 	return df
 	
-def load_historical_data(d1=datetime.datetime(2018,2,3), d2 = datetime.datetime(2018,5,2), region="sa1"):
-	#convert 
-	d1_isocal = dt_to_isocal(d1)	
-	d2_isocal = dt_to_isocal(d2)
-	print (d2_isocal)	
-	print (isocal_to_dt(d2_isocal))
+def historical_weeks(d1=datetime.datetime(2018,2,3), d2 = datetime.datetime(2018,5,2), region="sa1"):
+	dt_i = dt_to_weekstart(d1)
+	while dt_i < d2:	
+		yield load_week(dt_i, region=region)
+		dt_i += datetime.timedelta(7)
+		
+def load_data(d1=datetime.datetime(2018,2,5), d2 = datetime.datetime(2018,2,6), region="sa1", split=True):	
+	series_list = [series for series in historical_weeks(d1=d1,d2=d2,region=region)]
+	df = pd.concat(series_list)
+	df.sort_index(inplace=True)
 	
+	#assert no duplicated index
+	assert len(df.index) == len(set(df.index))
+
+	dx = df[(df.index>d1) & (df.index<=d2)].copy()
+
+	if split:
+		return split_df(dx)
+	else:
+		return dx
+	
+def split_df(df):
+	_30min_series = ["PRICE", "TEMPERATURE", "ROOFTOP_SOLAR"]
+	df_30 = pd.concat([df[series].dropna() for series in _30min_series], axis=1)	
+	df.drop(_30min_series, inplace=True, axis=1)	
+	return df, df_30
+		
 def load_week(dt_start, region='sa1'):	
 	filename = "{0}_{1}.csv".format(region,dt_start.strftime("%Y%m%d"))
-	filepath = os.path.join(data_dir,filename)
+	folder = "power"
+	filepath = os.path.join(data_dir,"power",filename)
 	if os.path.exists(filepath):	
 		return pd.read_csv(filepath,index_col=0,parse_dates=[0])
 	else:
@@ -31,9 +54,33 @@ def load_week(dt_start, region='sa1'):
 
 def download_week(dt_start, region='sa1'):
 	iso_week = dt_to_isocal(dt_start)
-	url = "http://data.opennem.org.au/power/history/5minute/{0}_{1}.json".format(region,iso_week)
+	url = history_url.format(region,iso_week)
 	r = requests.get(url)
-	return 	pd.concat([json_to_pd(data_set) for data_set in data_sets(r)], axis=1)
+	return 	merge_series(r)
+	
+def merge_series(r):
+	df = pd.concat([json_to_pd(data_set) for data_set in data_sets(r)], axis=1)	
+	merge_and_drop_ic(df)
+	if ("BATTERY_CHARGING" in df) or ("BATTERY_DISCHARGING" in df):
+		merge_and_drop_batt(df)	
+	return standardised_columns(df)
+
+	
+def standardised_columns(df):
+	fuel_tech = ["BIOMASS","BLACK_COAL", "BROWN_COAL", "DISTILLATE", "GAS_CCGT", "GAS_OCGT", "GAS_RECIP", "GAS_STEAM", "HYDRO", "PUMPS", "SOLAR", "WIND", "BATTERY"]
+	for col in fuel_tech:
+		if col not in df:
+			df[col] = pd.np.nan
+	col_order = ["DEMAND", "NETINTERCHANGE"] + sorted(fuel_tech) + ["PRICE", "TEMPERATURE", "ROOFTOP_SOLAR"]
+	return df[col_order]
+	
+def merge_and_drop_ic(df):
+	df["NETINTERCHANGE"] = df[["EXPORTS", "IMPORTS"]].sum(axis=1)
+	df.drop(["EXPORTS", "IMPORTS"], axis=1,inplace=True)	
+
+def merge_and_drop_batt(df):
+	df["BATTERY"] = df["BATTERY_DISCHARGING"] - df["BATTERY_CHARGING"]
+	df.drop(["BATTERY_DISCHARGING", "BATTERY_CHARGING"], axis=1,inplace=True)	
 
 def dt_to_weekstart(dt):
 	#convert dt to datetime of start of week
@@ -61,7 +108,6 @@ def json_to_pd(data_set):
 		name = _type.upper()	
 	else:
 		name = _id.split(".")[-2].upper()
-	
 	dt_i = datetime.datetime.strptime(series['start'],'%Y-%m-%dT%H:%M+1000') 
 	dt_f = datetime.datetime.strptime(series['last'],'%Y-%m-%dT%H:%M+1000')
 	interval = series['interval']		
@@ -74,5 +120,5 @@ def json_to_pd(data_set):
 	return pd.DataFrame(index=index, data = series['data'], columns=[name])
 
 def load_all_regions():
-	return {region: load_week(region) for region in ['nsw', 'qld', 'sa','tas','vic','nem']}
+	return {region: load_week(region) for region in ['nsw', 'qld', 'sa','tas','vic']}
 	
