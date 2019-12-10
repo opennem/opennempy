f366b699a4fe8e12c0f0509d5c9d80a1c19cdbf3
Ben Elliston <bje@air.net.au>
2019-12-11 00:57:44 +1100

    Remove RE penetration stuff.

--- emissions/plot.py
+++ emissions/plot.py
@@ -10,9 +10,6 @@ from numpy import arange
 public_html = '/home/bje/public_html/'
 nrows = 288 * 7
 
-repen = pd.read_csv('re-pen.log', sep=',', parse_dates=True, index_col='Time')
-repen = repen.tail(nrows)
-
 ei = pd.read_csv('ei.log', sep=',', parse_dates=True, index_col='Time')
 ei = ei.tail(nrows)
 
@@ -20,13 +17,6 @@ fig2, ax2 = plt.subplots(nrows=1, ncols=2)
 fig2.set_size_inches(20, 8)
 plt.subplots_adjust(wspace=0.2)
 plt.suptitle('National Electricity Market 5-min renewable share and emissions intensity\n\nData sourced from AREMI and APVI')
-
-repen.plot(ax=ax2[0])
-ax2[0].set_ylabel("Renewable share (%)")
-ax2[0].xaxis.set_major_locator(DayLocator())
-ax2[0].xaxis.set_minor_locator(HourLocator(arange(0, 25, 12)))
-ax2[0].xaxis.set_minor_formatter(DateFormatter("%p"))
-ax2[0].xaxis.set_major_formatter(DateFormatter('%Y-%m-%d   '))
 
 ei.plot(ax=ax2[1])
 ax2[1].set_ylabel("Emissions intensity (t CO2/MWh)")
