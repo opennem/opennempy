dbf711b22bb4da51aab9d4c35b4c19cc816602a9
Nik Cubrilovic <git@nikcub.me>
2021-03-19 17:39:24 +1100

    FEAT: Parse environment + unit tests

---
+++ opennem/core/environment.py
@@ -0,0 +1,21 @@
+from opennem.schema.envs import Environment
+
+
+class EnvironmentNotFound(Exception):
+    pass
+
+
+def get_environment(environment_name: str) -> Environment:
+    """ Returns an environment type definition from a string environment name """
+    en = environment_name.strip().lower()
+
+    if en in ["dev", "development", "local"]:
+        return Environment.development
+
+    if en in ["stage", "staging"]:
+        return Environment.staging
+
+    if en in ["prod", "production"]:
+        return Environment.production
+
+    raise EnvironmentNotFound("Could not find environment: {}".format(environment_name))
