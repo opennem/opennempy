5c5bbac261f5ed35a4bf602caf3d1fc13a0498c7
Nik Cubrilovic <git@nikcub.me>
2021-03-24 09:58:38 +1100

    FEAT: Lint on release

--- scripts/make_release.sh
+++ scripts/make_release.sh
@@ -8,6 +8,7 @@ flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
 flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 
 # mypy
+# mypy opennem
 
 poetry version ${1-prerelease}
 
