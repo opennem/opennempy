3373372d4ad628ecdb89857a2b557c1070b4e19f
Nik Cubrilovic <git@nikcub.me>
2021-03-23 22:28:51 +1100

    FEAT: Networks schema

---
+++ opennem/schema/network.py
@@ -0,0 +1,24 @@
+from typing import List, Optional
+
+from pydantic import Field
+
+from .core import BaseConfig
+
+
+class NetworkRegionSchema(BaseConfig):
+    """ Defines a network region """
+
+    code: str
+    timezone: Optional[str] = Field(None, description="Network region timezone")
+
+
+class NetworkSchema(BaseConfig):
+    """ Defines a network """
+
+    code: str
+    country: str
+    label: str
+
+    regions: Optional[List[NetworkRegionSchema]]
+    timezone: Optional[str] = Field(None, description="Network timezone")
+    interval_size: int = Field(..., description="Size of network interval in minutes")
