a1ff8b91469ebc73480a0106d1095c5e5e026600
Nik Cubrilovic <git@nikcub.me>
2021-03-25 12:40:00 +1100

    FEAT: sphinx-rtd-theme

--- source/conf.py
+++ source/conf.py
@@ -1,9 +1,3 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -27,7 +21,7 @@ author = "OpenNEM"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["myst_parser", "sphinx.ext.autodoc"]
+extensions = ["myst_parser", "sphinx.ext.autodoc", "sphinx_rtd_theme"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
@@ -38,39 +32,15 @@ templates_path = ["_templates"]
 exclude_patterns = []
 
 
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
-html_theme = "pydata_sphinx_theme"
+html_theme = "sphinx_rtd_theme"
 html_logo = "_static/logo.png"
 
 html_theme_options = {
-    "icon_links": [
-        {
-            "name": "GitHub",
-            "url": "https://github.com/opennem/opennempy",
-            "icon": "fab fa-github-square",
-        },
-        {
-            "name": "Twitter",
-            "url": "https://twitter.com/opennem",
-            "icon": "fab fa-twitter-square",
-        },
-        {
-            "name": "PyPI",
-            "url": "https://pypi.org/project/opennem/",
-            "icon": "fas fa-box",
-        },
-    ],
-    "external_links": [
-        {"name": "OpenNEM", "url": "https://opennem.org.au"},
-        {"name": "OpenAPI Docs", "url": "https://api.opennem.org.au/docs"},
-    ],
+    "style_nav_header_background": "#ece9e6",
+    "logo_only": True,
+    "collapse_navigation": False,
+    "includehidden": True,
+    "github_url": "https://github.com/opennem/opennem/py",
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
